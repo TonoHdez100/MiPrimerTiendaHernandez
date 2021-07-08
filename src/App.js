7a3435260b9b6216985880905930cba4c1fb07d1
Antonio <bernabe.hernandez64@gmail.com>
2021-07-07 20:16:36 -0500

    new features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from "./Components/NavBar"
+import CartComponent from './Components/CartComponent';
+
 
 function App() {
   return (
     <div className="container">
       <br />
       <NavBar />
+      <CartComponent />
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,10 @@
 import react            from "react"
 import Navbar           from "react-bootstrap/Navbar"
 import Nav              from "react-bootstrap/Nav"
-import FontAwesomeIcon  from '@fortawesome/react-fontawesome'
-import {faBell} from "@fortawesome/free-solid-svg-icons"
+import CarWidget        from './CarWidget';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
@@ -12,15 +12,13 @@ export default function NavBar(){
     return(
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">Mi Primer Tienda</Navbar.Brand>
-            <Nav className="mr-auto">
+            <Nav>
                 <Nav.Link href="#home">Inicio</Nav.Link>
                 <Nav.Link href="#whos">Quienes Somos</Nav.Link>
                 <Nav.Link href="#products">Productos</Nav.Link>
-                <Nav.Link href="#carrito">Mi Carrito</Nav.Link>
+                <Nav.Link href="#carrito"><CarWidget /> </Nav.Link>
             </Nav>
         </Navbar>
-        
-
 
     )
 }
